89dc21f3030bf994fc500aa2cecf3d2f33a5deff
Jeremy Nguyen Xuan <jeremy.nguyenxuan@gottexbrokers.com>
2013-05-21 16:21:27 +0200

    Moved src/java to src/scala

--- src/main/scala/org/scalafin/date/SimplePeriod.scala
+++ src/main/scala/org/scalafin/date/SimplePeriod.scala
@@ -1,7 +1,6 @@
 package org.scalafin.date
 
 import org.joda.time.DateMidnight
-import scala.swing.Orientable
 
 case class SimplePeriod (dateRange: DateRange, originalDateRange: Option[DateRange] = None) extends DatePeriod
 
