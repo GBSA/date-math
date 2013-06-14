e33b727de09acf016bd4d75f05810f7f6f9f9c76
Jeremy Nguyen Xuan <jeremy.nguyenxuan@gottexbrokers.com>
2013-06-14 14:16:54 +0200

    added ScheduleImports.scala

---
+++ src/main/scala/org/scalafin/scheduler/ScheduleImports.scala
@@ -0,0 +1,6 @@
+package org.scalafin.scheduler
+
+import org.scalafin.date.DateOrdering
+import org.scalafin.date.DatePeriodOpsFunc
+
+object ScheduleImports extends DatePeriodOpsFunc with DateOrdering
