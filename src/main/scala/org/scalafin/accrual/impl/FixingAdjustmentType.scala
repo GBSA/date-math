2154b6960230544a871d2c8546f351714c9f8a5e
Jeremy Nguyen Xuan <jeremy.nguyenxuan@gottexbrokers.com>
2013-06-14 09:16:58 +0200

    Accrual is not used in Gottware and the specifications are not clear. We'll leave it out for the moment.

--- src/main/scala/org/scalafin/accrual/impl/FixingAdjustmentType.scala
+++
@@ -1,6 +0,0 @@
-package org.scalafin.accrual.impl
-
-
-sealed abstract class FixingAdjustmentType
-case object CALENDAR_DAYS extends FixingAdjustmentType
-case object BUSINESS_DAYS extends FixingAdjustmentType
