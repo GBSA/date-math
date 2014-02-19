7f86af2dc24ea9e868ca24cb47fe18ff4235d90a
edmondo1984 <edmondo.porcu@gmail.com>
2014-02-19 16:11:06 +0100

    Major refactoring + killed useless classes

--- src/main/scala/org/scalafin/date/SimplePeriod.scala
+++
@@ -1,7 +0,0 @@
-package org.scalafin.date
-
-import org.joda.time.DateMidnight
-
-case class SimplePeriod (dateRange: DateRange, originalDateRange: Option[DateRange] = None) extends DatePeriod
-
-object SimplePeriod
--- src/main/scala/org/scalafin/scheduler/ScheduleImports.scala
+++
@@ -1,6 +0,0 @@
-package org.scalafin.scheduler
-
-import org.scalafin.date.DateOrdering
-import org.scalafin.date.DatePeriodOpsFunc
-
-object ScheduleImports extends DatePeriodOpsFunc with DateOrdering
